170c71672523eee11ca13a0e1bae28ed479433e5
lukasz-brzozko <50025154+lukasz-brzozko@users.noreply.github.com>
2024-07-18 19:15:14 +0200

    Initial commit

---
+++ src/index.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Userscript starter
+// @namespace    https://github.com/lukasz-brzozko/userscript-starter
+// @version      2024-05-21
+// @description  Creates a new userscript
+// @author       Łukasz Brzózko
+// @match        https://github.com/
+// @icon         https://github.githubassets.com/favicons/favicon.png
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.user.js
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const init = () => {};
+
+  init();
+})();
